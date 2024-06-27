095703b9e3f2ab9249c883f659dfd77a45087b76
MohamedNagy0 <mohamednagy.mis@gmail.com>
2024-06-27 18:24:22 +0300

    update logout path

--- src/Context/User.context.jsx
+++ src/Context/User.context.jsx
@@ -52,7 +52,7 @@ export default function UserProvider({ children }) {
         localStorage.removeItem("token");
         setTimeout(() => {
             toast.dismiss(toastId);
-            window.location.href = "http://localhost:5174/";
+            window.location.href = "http://localhost:5173/";
             setToken(null);
             toast(<span className="text-darkPrimary ">Logged out</span>, {
                 duration: 2000,
